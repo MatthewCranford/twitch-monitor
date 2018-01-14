59d7923db22065538e20a028b5859ec1a2b3f45f
Matt <matthewcranford314@gmail.com>
2018-01-13 16:26:12 -0800

    Connect API

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
   
-  var user = "freecodecamp";
-  var url = "https://wind-bow.gomix.me/twitch-api/users/" + user;
-  console.log(url);
- 
+  var users = ["freecodecamp", "esl_sc2"];
 
-  $.ajax({
-    url: url,
-    dataType: "jsonp",
-    type: 'GET',
-    success: function(data) {
-      console.log(data);
-    }
-  });
+  $.each(users, function(i) {
+    console.log(users[i]);
+    var url = "https://wind-bow.gomix.me/twitch-api/users/" + users[i];
+    console.log(url);
+    $.ajax({
+      url: url,
+      dataType: "jsonp",
+      type: 'GET',
+      success: function(data) {
+        $(".logo").attr("src",data.logo); 
+        $(".display-name").html(data.display_name);
+        var id = data._id;
+        console.log(data._id);
+        var url = "https://wind-bow.gomix.me/twitch-api/streams/" + id;
+        console.log(url);
+        $.ajax({
+          url: url,
+          dataType: "jsonp",
+          type: 'GET',
+          success: function(data) {
+            if(data.stream === null) {
+              $(".status").html("Offline");
+            }
+            else {
+              $(".status").html(data.channel.status);
+            }
+          }
+        });
+      }
+    });
+  })
 });
