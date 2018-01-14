c70606ee08772c862cc86e60b1711efb8b115ecf
Matt <matthewcranford314@gmail.com>
2018-01-14 15:37:47 -0800

    Add stream status

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
 $(document).ready(function() {
   
-  var users = ["freecodecamp", "esl_sc2"];
+  var users = ["esl_sc2", "freecodecamp"];
+  var streamerList = $("#streamer-list");
 
   $.each(users, function(i) {
-    console.log(users[i]);
-    var url = "https://wind-bow.gomix.me/twitch-api/users/" + users[i];
-    console.log(url);
-    $.ajax({
-      url: url,
-      dataType: "jsonp",
-      type: 'GET',
-      success: function(data) {
+    $('#streamer-list').append($("<li>").append($("<img>").attr("class", "logo")));
+
+    // console.log(users[i]);
+    var url = "https://wind-bow.gomix.me/twitch-api/users/" + users[i] + "/?callback=?";
+     console.log(url);
+    $.getJSON(url, function(data) {
         $(".logo").attr("src",data.logo); 
         $(".display-name").html(data.display_name);
         var id = data._id;
-        console.log(data._id);
-        var url = "https://wind-bow.gomix.me/twitch-api/streams/" + id;
-        console.log(url);
-        $.ajax({
-          url: url,
-          dataType: "jsonp",
-          type: 'GET',
-          success: function(data) {
+        var url = "https://wind-bow.gomix.me/twitch-api/streams/" + users[i] + "/?callback=?";
+        // console.log(url);
+        $.getJSON(url, function(data) {
             if(data.stream === null) {
               $(".status").html("Offline");
             }
             else {
-              $(".status").html(data.channel.status);
+              $(".status").html(data.stream.channel.status);
             }
-          }
         });
-      }
     });
-  })
+  });
 });
