762c3b6499f98a5e86fbc670c318fd52077c5a17
Cranford <Matthew.Cranford@lamresearch.com>
2018-01-15 19:04:18 -0800

    Work on streamer list

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ $(document).ready(function() {
   var users = ["esl_sc2", "freecodecamp"];
   var streamerList = $("#streamer-list");
 
-  $.each(users, function(i) {
-    $('#streamer-list').append($("<li>").append($("<img>").attr("class", "logo")));
+  for (user in users) {
+    console.log(users[user]);
+    $("#streamer-list").append($("<li>").append($("<img>").attr("class", "logo")));
 
-    // console.log(users[i]);
-    var url = "https://wind-bow.gomix.me/twitch-api/users/" + users[i] + "/?callback=?";
+    
+    var url = "https://wind-bow.gomix.me/twitch-api/users/" + users[user] + "/?callback=?";
      console.log(url);
+     
     $.getJSON(url, function(data) {
+        console.log("enter json");
         $(".logo").attr("src",data.logo); 
         $(".display-name").html(data.display_name);
         var id = data._id;
-        var url = "https://wind-bow.gomix.me/twitch-api/streams/" + users[i] + "/?callback=?";
-        // console.log(url);
+        var url = "https://wind-bow.gomix.me/twitch-api/streams/" + users[user] + "/?callback=?";
+        console.log(url);
         $.getJSON(url, function(data) {
             if(data.stream === null) {
               $(".status").html("Offline");
@@ -24,5 +27,5 @@ $(document).ready(function() {
             }
         });
     });
-  });
+  };
 });
