2ead2112eb2a5f0967ac2c4d98114ce00e845f6e
MatthewCranford <matthewcranford314@gmail.com>
2018-01-23 16:54:44 -0800

    Work on add streamer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   
-  var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "boozycruzy"];
   var streamerList = $("#streamer-list");
   for (var i = 0; i < users.length; i++) {
     (function(i) {
@@ -8,21 +8,23 @@ $(document).ready(function() {
       $("#streamer-list").append($("<li>").attr("id", i).attr("class", "streamer-list-item"));
 
       $("#" + i).append($("<img>").attr("id", "logo" + i).attr("class", "logo"));
-      $("#" + i).append($("<span>").attr("id", "display-name" + i).attr("class", "display-name"));
+      $("#" + i).append($("<a>").attr("id", "link" + i).attr("target", "_blank"));
+      $("#link" + i).append($("<span>").attr("id", "display-name" + i).attr("class", "display-name"));
       $("#" + i).append($("<span>").attr("id", "status" + i).attr("class", "status"));
 
       var url = "https://wind-bow.gomix.me/twitch-api/users/" + users[i] + "/?callback=?";
-      console.log(url); 
+      // console.log(url); 
       
       $.getJSON(url, function(data) {
         // console.log("entering json");
         $("#logo" + i).attr("src",data.logo); 
+        $("#link" + i).attr("href", "https://www.twitch.tv/" + data.display_name)
         $("#display-name" + i).html(data.display_name);
         var id = data._id;
         var url = "https://wind-bow.gomix.me/twitch-api/streams/" + users[i] + "/?callback=?";
-        console.log(url);
+        // console.log(url);
         $.getJSON(url, function(data) {
-          console.log(url +"entering 2nd json");
+          // console.log(url +"entering 2nd json");
             if(data.stream === null) {
               $("#status" + i).html("Offline");
             }
@@ -46,13 +48,22 @@ $(document).ready(function() {
     });
   }
 
-  
-
-  $("#all-container").click(function() {
-    $("#streamer-list li").each(function(index) {
-      $(this).css("display","flex");
-    });
+  $("#streamer-add-btn").click(function() {
+    $("#streamer-add-input").css("display", "flex");
+    $(this).css("margin-right", "inherit");
   });
+
+  $("#streamer-add-input").submit(function() {
+    
+    var input = $("#streamer-add-input").val();
+    console.log("input"); 
+  });
+
+  // $("#all-container").click(function() {
+  //   $("#streamer-list li").each(function(index) {
+  //     $(this).css("display","flex");
+  //   });
+  // });
 
   $("#online-container").click(function() {
     $("#streamer-list li").each(function(index) {
@@ -82,17 +93,9 @@ $(document).ready(function() {
 
 
   $(".tab-container").click(function() {
-    // $(".tab-text").css("display", "none");
-    // $(this).find(".tab-text").css("display", "block");
     $(".tab-container").removeClass("toggled-tab");
     $(".tab-container").find(":nth-child(2)").removeClass("toggled-text");
     $(this).addClass("toggled-tab");
     $(this).find(":nth-child(2)").addClass("toggled-text");
   });
-
-  // $(".tab-container").mouseover(function() {
-  //   $(".tab-container").css("width", "15px" );
-  //   $(this).css("width", "65px" );
-    
-  // })
 });
